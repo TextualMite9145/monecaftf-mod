e2cef0e211dc6d2e509eb5642351d3969189c333
TextualMite9145 <steinmannale@student.carey.wa.edu.au>
2025-01-15 18:15:08 +0800

    uploadtemplate

---
+++ src/main/java/net/textualmite9145/villagertower/VillagerWatchTowerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.textualmite9145.villagertower;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VillagerWatchTowerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
